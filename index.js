8961e57e5d64e37d33e1b6281d5c92732231495d
AbraaoAzP <abraaoazp@gmail.com>
2024-01-16 14:10:00 -0300

    projeto em desenvolvimento

---
+++ index.js
@@ -0,0 +1,11 @@
+let victoryAmount = rankedCalculator(30, 15)
+let heroName = "Frederico"
+
+function rankedCalculator(victory, loss) {
+    return victory - loss
+}
+
+function rankedLevel(victoryAmount) { 
+}
+
+console.log("O herói " + heroName + " tem o saldo de " + victoryAmount + " vitórias")
