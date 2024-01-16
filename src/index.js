3f49f6527824fc90c01bec08dee8820777690e0a
AbraaoAzP <abraaoazp@gmail.com>
2024-01-16 14:50:51 -0300

    Projeto Finalizado

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 let victoryAmount = rankedCalculator(147, 53)
 let heroName = "Frederico"
-let rankLevel = ""
+let rankLevel = levelCalculator(victoryAmount)
 
 function rankedCalculator(victory, loss) {
     return victory - loss
 }
 
-if (victoryAmount < 10) {
-    rankLevel = "Ferro"
-} else if (victoryAmount > 10 && victoryAmount <= 20) {
-    rankLevel = "Bronze"
-} else if (victoryAmount > 20 && victoryAmount <= 50) {
-    rankLevel = "Prata"
-} else if (victoryAmount > 50 && victoryAmount <= 80) {
-    rankLevel = "Ouro"
-} else if (victoryAmount > 80 && victoryAmount <= 90) {
-    rankLevel = "Diamante"
-} else if (victoryAmount > 90 && victoryAmount <= 100) {
-    rankLevel = "Lendário"
-} else {
-    rankLevel = "Imortal"
+function levelCalculator (victoryAmount) {
+    if (victoryAmount < 10) {
+    return "Ferro"
+    } else if (victoryAmount > 10 && victoryAmount <= 20) {
+    return "Bronze"
+    } else if (victoryAmount > 20 && victoryAmount <= 50) {
+    return  "Prata"
+    } else if (victoryAmount > 50 && victoryAmount <= 80) {
+    return "Ouro"
+    } else if (victoryAmount > 80 && victoryAmount <= 90) {
+    return "Diamante"
+    } else if (victoryAmount > 90 && victoryAmount <= 100) {
+    return "Lendário"
+    } else {
+    return "Imortal"
+    }
 }
-
 console.log("O herói " + heroName + " tem o saldo de " + victoryAmount + " vitórias e está no nível " + rankLevel)   
